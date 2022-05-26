95929e1234058bbeea67bcbda147e50cce2ac1cf
luyh <luyanhuan@tinypace.com>
2022-05-26 18:17:57 +0800

    commit

---
+++ sway/lockman.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Times the screen off and puts it to background
+# Locks the screen immediately
+# Kills last background task so idle timer doesn't keep running
+#swaylock --clock --indicator --effect-blur 7x5 --screenshots --effect-scale 0.4 --effect-vignette 0.2:0.5 --effect-blur 4x2 --datestr "%a %e.%m.%Y" --timestr "%k:%M"
+
+
+
+swaylock \
+    --screenshots \
+    --clock \
+    --indicator \
+    --indicator-radius 100 \
+    --indicator-thickness 7 \
+    --effect-blur 7x5 \
+    --effect-vignette 0.5:0.5 \
+    --ring-color bb00cc \
+    --key-hl-color 880033 \
+    --line-color 00000000 \
+    --inside-color 00000088 \
+    --separator-color 00000000 \
+    --grace 2 \
+    --fade-in 0.2
